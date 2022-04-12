40469861395cbbdf124aadb467586dcb20867996
mirembe jemimah <jaymiecrystal117@gmail.com>
2022-04-12 18:10:01 +0300

    mtn clone

---
+++ assests/drop.js
@@ -0,0 +1,3 @@
+var parentDropDown = document.querySelectorAll('.parent-dropdown');
+var mobileTopHeaderDropDown = document.querySelector('.mobile-dropdowns');
+var dropDownLabel = document.querySelector('.droppdowns-labels');
